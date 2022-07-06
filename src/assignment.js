edc0c6e1c182d30c893b8436c8636d18b4df5226
OGUNJIMI OLAMIDE <ogunjimi39@gmail.com>
2022-07-06 08:44:32 +0000

    Attempt 1 and 3

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for ( let i = 0; i <arrayOfNumbers.length; i++){
+    sum += arrayOfNumbers[i]
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -47,9 +51,17 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  const newValues = [];
+
+  arrayOfNumbers.forEach (number => {
+    let num = Math.floor((number * 1.8) + 32)
+    newValues.push(num)
+    
+  });
+  
+    return newValues;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+ assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
